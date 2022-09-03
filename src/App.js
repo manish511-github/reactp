0c3c828ba8560c9c8a2cd240320224d41bd6659e
manish511-github <76744029+manish511-github@users.noreply.github.com>
2022-09-03 17:25:39 +0530

    Fucntion and Class Click

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import Greet from './components/Greet';
 import Welcome from './components/Welcome'
 import Message from './components/Message';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick'
+import ClassClick from './components/ClassClick';
 export default class App extends Component {
   render() {
     return (
    <div className="App">
-    
+    <FunctionClick/>
+
+    <ClassClick/>
  <Counter/>
     {/* <Greet name="Bruce" heroName="Batsman" >
     <p>This is children props</p>
