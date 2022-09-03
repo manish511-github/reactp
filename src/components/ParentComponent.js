d6a37450d53809fa9b0f5e0f73f9badb6b33c90a
manish511-github <76744029+manish511-github@users.noreply.github.com>
2022-09-03 18:10:14 +0530

    Methods as Props II

--- src/components/ParentComponent.js
+++ src/components/ParentComponent.js
@@ -9,8 +9,8 @@ export default class ParentComponent extends Component {
         }
         this.greetParent=this.greetParent.bind(this)
     }
-    greetParent(){
-        alert(`Hello ${this.state.parentName}`)
+    greetParent(childName){
+        alert(`Hello ${this.state.parentName} from ${childName}`)
     }
   render() {
     return (
