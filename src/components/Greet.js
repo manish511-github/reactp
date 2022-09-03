d2fa96cefd4a243e66d38731bc774a0a867b9b4e
manish511-github <76744029+manish511-github@users.noreply.github.com>
2022-09-03 13:54:13 +0530

    Destuction prop and state

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,12 +1,13 @@
 
 import React from "react";
-const Greet=({name,heroName})=>{
+const Greet=props=>{
+    const {name,heroName}=props
    return (
     <div>
         <h1>
             Hello {name} a.k.a {heroName}
         </h1>
-      
+ 
     </div>
    )
 }
