f0026162c31ad4e1a3ee908e530763ab1f3c87d3
manish511-github <76744029+manish511-github@users.noreply.github.com>
2022-09-03 18:34:56 +0530

    NameList II

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 export default function NameList() {
     const names=['Bruce','Clark','Diana']
+    const nameList=
+    names.map(name=> <h2>{name}</h2>)
   return (
   <div>
-    {
-        names.map(name=> <h2>{name}</h2>)
+    {nameList
     }
   </div>
   )
