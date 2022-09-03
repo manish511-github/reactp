2a5fd5f7e4ba27fc6b1368aa81135c701e3940e5
manish511-github <76744029+manish511-github@users.noreply.github.com>
2022-09-03 18:41:27 +0530

    Person List

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,12 +1,38 @@
 import React from 'react'
 
 export default function NameList() {
-    const names=['Bruce','Clark','Diana']
-    const nameList=
-    names.map(name=> <h2>{name}</h2>)
+    const persons=[{
+        id:1,
+        name:'Bruce',
+        age:30,
+        skill:'React'
+    },
+    {
+        id:2,
+        name:'Clark',
+        age:25,
+        skill:'Angular'
+    },
+    {
+        id:3,
+        name:'Diana',
+        age:25,
+        skill:'Vue'
+    }
+
+
+]
+    const personList=
+    persons.map(person=> <h2>
+        I am {
+            person.name
+        }. I am {person.age} years old. I know {person.skill}
+
+
+    </h2>)
   return (
   <div>
-    {nameList
+    {personList
     }
   </div>
   )
